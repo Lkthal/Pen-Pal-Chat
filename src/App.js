fc26143b3bc8cf2e0b9b82396e463e0cb914cb6a
Thal Syhabout <tsyhabout@gmail.com>
2018-02-08 23:01:27 -0800

    complete chat room

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RoomList from './components/RoomList';
 import * as firebase from 'firebase';
 
 var config = {
